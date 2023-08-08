477e9030442cbac90cd8f389c357fd78cf46cb9c
cmdoret <cyril.mattheydoret@gmail.com>
2023-08-08 21:03:39 +0200

    feat(enhance): functional workflow

--- src/enhance.py
+++ src/enhance.py
@@ -1,47 +1,70 @@
-from functools import reduce
-from pathlib import Path
+"""Enhance the metadata with additional information and extract to CSV for visualization."""
 
-from gimie.project import Project
-from dotenv import load_dotenv
+from pathlib import Path
+from io import StringIO
+
+import pandas as pd
 from prefect import flow, task
 from rdflib import Graph
-from retrieve import read_papers
 
 from config import Location
+from retrieve import read_papers
 
 EXTRACT_QUERY = """
 
 PREFIX schema: <http://schema.org/>
 
-SELECT ?url ?name ?forks (COUNT(?contributor) as ?contributors) ?license ?created_at ?updated_at
+SELECT ?url ?lang (COUNT(?contrib) as ?contributors) ?license ?created_at ?updated_at
 WHERE {
     ?url a schema:SoftwareSourceCode ;
-        schema:name ?name ;
-        schema:license ?license ;
+        schema:programmingLanguage ?lang ;
         schema:dateCreated ?created_at ;
         schema:dateModified ?updated_at .
-
-    ?contributor a schema:Person ;
-        schema:contributor ?url .
-}"""
+    OPTIONAL {
+        ?url schema:license ?license ;
+    }
+    OPTIONAL {
+        ?url schema:contributor ?contrib .
+    }
+}
+GROUP BY ?url"""
 
 
 @task
 def graph_to_table(graph: Graph) -> str:
-    csv = graph.query(EXTRACT_QUERY).serialize(format="csv")
+    csv = graph.query(EXTRACT_QUERY).serialize(format="csv").decode()
     return csv
 
 
 @task
 def save_table(csv: str, target_path: Path):
-    with open(target_path, "wt") as f:
+    with open(target_path, "w") as f:
         f.write(csv)
 
 
+@flow
 def enhance_flow(location: Location = Location()):
-    meta_graph = Graph().parse(location.metadata, format="turtle")
-    meta_table = graph_to_table(meta_graph)
-    save_table(meta_table, location.meta_table)
+    """Extract table from repository metadata
+    and merge with additional information"""
+
+    # Load repo metadata (Github)
+    meta_graph = Graph().parse(location.repo_rdf, format="turtle")
+    meta_csv = graph_to_table(meta_graph)
+    meta_df = pd.read_csv(StringIO(meta_csv))
+
+    # Load paperswithcode data
+    papers = read_papers(location.pwc_filtered_json)
+    papers_df = pd.DataFrame(papers)
+
+    # Load license metadata
+
+    # Link datasets
+    enhanced_df = pd.merge(
+        meta_df, papers_df, left_on="url", right_on="repo_url"
+    )
+
+    # Persist to disk
+    enhanced_df.to_csv(location.combined_csv, index=False)
 
 
 if __name__ == "__main__":
