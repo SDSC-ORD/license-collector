c61f7837ddb0efb4c902dfb03de41b8820b41ace
cmdoret <cyril.mattheydoret@gmail.com>
2023-08-08 21:03:16 +0200

    refactor(config): more meaningfule output names

--- src/config.py
+++ src/config.py
@@ -9,13 +9,13 @@ class Location(BaseModel):
     """Specify the locations of inputs and outputs"""
 
     pwc_url: str = "https://production-media.paperswithcode.com/about/links-between-papers-and-code.json.gz"
-    all_papers: str = Path("data/raw/papers_with_code.json.gz")
-    filtered_papers: str = Path("data/processed/filtered_papers.json.gz")
-    metadata: str = Path("data/processed/projects_meta.ttl")
-    meta_table: str = Path("data/processed/projects_meta.csv")
+    pwc_json: str = Path("data/raw/papers_with_code.json.gz")
+    pwc_filtered_json: str = Path("data/processed/filtered_papers.json.gz")
+    repo_rdf: str = Path("data/processed/repo_meta.ttl")
+    combined_csv: str = Path("data/final/combined_metadata.csv")
 
 
 class Config(BaseModel):
     """Specify parameters for the flow"""
 
-    max_papers: int | None = 100
+    max_papers: int | None = 1000
