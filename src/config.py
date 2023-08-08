4b41eda1b255ab913cb88adaa6edf87d856e2500
cmdoret <cyril.mattheydoret@gmail.com>
2023-08-08 19:54:00 +0200

    refactor: str -> pathlib.Path

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 """
 create Pydantic models
 """
+from pathlib import Path
 from pydantic import BaseModel
 
 
@@ -8,8 +9,13 @@ class Location(BaseModel):
     """Specify the locations of inputs and outputs"""
 
     pwc_url: str = "https://production-media.paperswithcode.com/about/links-between-papers-and-code.json.gz"
-    all_papers: str = "data/raw/papers_with_code.json.gz"
-    filtered_papers: str = "data/processed/filtered_papers.json.gz"
-    metadata: str = "data/processed/projects_meta.ttl"
-    meta_table: str = "data/processed/projects_meta.csv"
+    all_papers: str = Path("data/raw/papers_with_code.json.gz")
+    filtered_papers: str = Path("data/processed/filtered_papers.json.gz")
+    metadata: str = Path("data/processed/projects_meta.ttl")
+    meta_table: str = Path("data/processed/projects_meta.csv")
 
+
+class Config(BaseModel):
+    """Specify parameters for the flow"""
+
+    max_papers: int | None = 100
--- src/retrieve.py
+++ src/retrieve.py
@@ -4,15 +4,16 @@
 import requests
 import gzip
 import json
+from pathlib import Path
 import re
 
 from prefect import flow, task  # type: ignore
 
-from config import Location
+from config import Config, Location
 
 
 @task
-def download_paper_list(url: str, target_path: str):
+def download_paper_list(url: str, target_path: Path):
     """Download the list of projects from the PWC website"""
     response = requests.get(url, stream=True)
     with open(target_path, "wb") as f:
@@ -21,32 +22,37 @@ def download_paper_list(url: str, target_path: str):
 
 
 @task
-def read_papers(path: str) -> list[dict]:
+def read_papers(path: Path) -> list[dict]:
     """Read the paperswithcode from a gzipped json file"""
     with gzip.open(path, "r") as f:
         return json.load(f)
 
 
 @task
-def filter_papers(papers: list[dict]) -> list[dict]:
+def filter_papers(
+    papers: list[dict], max_papers: int | None = None
+) -> list[dict]:
     """Filter papers to only include those with a GitHub or GitLab repo"""
     repo_pattern = re.compile(r"^https?://(www\.)?(github|gitlab)\.com/")
+    if max_papers is not None:
+        papers = papers[:max_papers]
     filtered = filter(lambda x: re.search(repo_pattern, x["repo_url"]), papers)
     return list(filtered)
 
 
 @task
-def save_papers(papers: list[dict], target_path: str):
+def save_papers(papers: list[dict], target_path: Path):
     """Save the filtered paperswithcode to a gzipped json file"""
-    with gzip.open(target_path, "w") as f:
-        json.dumps(papers, f)
+    with gzip.open(str(target_path), "wt") as f:
+        json.dump(papers, f)
 
 
 @flow
-def retrieve_flow(location: Location = Location()):
-    download_paper_list(location.pwc_url, location.all_papers)
+def retrieve_flow(config: Config = Config(), location: Location = Location()):
+    if not location.all_papers.exists():
+        download_paper_list(location.pwc_url, location.all_papers)
     papers = read_papers(location.all_papers)
-    papers = filter_papers(papers)
+    papers = filter_papers(papers, max_papers=config.max_papers)
     save_papers(papers, location.filtered_papers)
 
 
